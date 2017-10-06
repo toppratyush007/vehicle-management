f3b8630d4ae56891d97558cb2939f830757dc3f7
vikash.k <vikash.k@media.net>
2017-10-06 13:08:55 +0530

    comment improved

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,10 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| @author :=> Vikash Kashyap <vikashkrkashyap@gmail.com>
+| @Since  :=> 5 september 2017
 */
 
-
-/**
- * @author :=> Vikash Kashyap <vikashkrkashyap@gmail.com>
- * @Since  :=> 5 september 2017
- */
 
 Route::get('/','MainController@homePage');
 Route::get('live/{deviceId}','MainController@showIndex');
